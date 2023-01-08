30999b84cc90e5d5e8e963fb21951aecaf58abd6
huro3h <huro3h@gmail.com>
2023-01-08 10:12:44 +0900

    CG-16 system spec(E2Eテスト)用の設定を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,41 +5,76 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
+require 'spec/spec_helper'
+require 'capybara/rspec'
+require 'rspec/rails'
+require 'factory_bot'
+
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
+Webdrivers.cache_time = 86400
+
+client = Selenium::WebDriver::Chrome::Remote::Http::Default.new
+browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |options|
+  options.args << '--headless'
+  options.args << '--disable-gpu'
+  options.args << '--disable-site-isolation-trials'
+  options.args << '--no-sandbox'
+  options.args << '--disable-dev-shm-usage'
+  options.args << '--window-size=1280,720'
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    capabilities: browser_options,
+    http_client: client
+  )
+end
+
 RSpec.configure do |config|
-  # FactoryBot. の接頭辞を省略する
-  config.include FactoryBot::Syntax::Methods
+  config.include Capybara::DSL, type: :system
+  config.include Rails.application.routes.url_helpers
+  config.include ActionDispatch::TestProcess::FixtureFile
+  %i[request view].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, type: type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
+    config.include ::Rails::Controller::Testing::Integration, type: type
+  end
+
+  config.include FactoryBot::Syntax::Methods # FactoryBot. の接頭辞を省略する
+
+  config.use_transactional_fixtures = true
+
+  config.before :each, type: :system do
+    driven_by :rack_test
+  end
+
+  config.before :each, type: :system, js: true do
+    # driven_by(:selenium_chrome)
+    driven_by :headless_chrome
+  end
+
+  config.before :each, type: :controller do
+    request.env['HTTP_HOST'] = 'localhost'
+    request.env['SERVER_PORT'] = 3000
+  end
+
+  config.before :suite do
+    FactoryBot.reload
+    Rails.application.load_tasks
+  end
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
