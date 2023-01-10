49b1ef29c2bb3683ce27d61e338cdfb7782c01d8
huro3h <huro3h@gmail.com>
2023-01-10 21:37:11 +0900

    CG-36 developを取り込んだ内容に合わせてテストを修正した

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -19,14 +19,16 @@ RSpec.describe UsersController, type: :request do
   describe 'POST #create' do
     context '正常に登録完了した時' do
       it 'ステータス302が返ること' do
-        post users_path params: { user: { name: "Sample Taro", password: 123456, password_confirmation: 123456 } }
+        post users_path params: {
+          user: { name: "Sample Taro", email: "sample_taro@example.com", password: 123456, password_confirmation: 123456 }
+        }
         expect(response.status).to eq 302
       end
     end
 
     context '登録項目に不備がある時' do
       it 'ステータス422が返ること' do
-        post users_path params: { user: { name: nil, password: nil, password_confirmation: nil } }
+        post users_path params: { user: { name: nil, password: "short", password_confirmation: "short" } }
         expect(response.status).to eq 422
         expect(response.body).to include 'The form contains 3 errors.'
         expect(response.body).to include 'Password is too short (minimum is 6 characters)'
