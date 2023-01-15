f98e4ee0b54988402e74034b175dc72a61b8d45d
kotaro watanabe <ktr.w1011@gmail.com>
2023-01-15 19:48:16 +0900

    セッション機能を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user&.authenticate(params[:session][:password])
-      redirect_to tops_url
+      log_in user
+      redirect_to tops_url || user
     else
       flash.now[:danger] = 'ユーザーが見つかりません'
       render 'new', status: :unprocessable_entity
@@ -13,11 +14,15 @@ class SessionsController < ApplicationController
 
   end
 
+  def destroy
+    log_out
+    redirect_to root_path
+  end
+
   private
     def user_params
         # parameterとして許可する属性定義
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-
 
 end
