72ac2600b05313281c48e0437d6ac39997c40675
huro3h <huro3h@gmail.com>
2023-01-16 00:44:11 +0900

    CG-42 ユーザの学習記録をユーザトップページに表示する

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,17 @@
 class TopsController < ApplicationController
   def index
+    set_user
+    set_study_records
+  end
+
+  private
+
+  def set_user
+    # session実装後に切り替えます
+    @user = User.first # User.find(params[:user_id]) # current_user
+  end
+
+  def set_study_records
+    @study_records = @user.study_records.preload(:subject).order(study_date: :desc).order(id: :desc)
   end
 end
