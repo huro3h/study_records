32fe88641a60b60e36e1e5514efc96fe4181065a
huro3h <huro3h@gmail.com>
2023-01-18 02:32:45 +0900

    CG-42 ログイン中ユーザのセッションにあわせて関連する勉強時間を表示するように修正した

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,19 +1,14 @@
 class TopsController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  
+  before_action :logged_in_user
+
   def index
-    set_user
     set_study_records
   end
 
   private
 
-  def set_user
-    # session実装後に切り替えます
-    @user = User.first # User.find(params[:user_id]) # current_user
-  end
-
   def set_study_records
-    @study_records = @user.study_records.preload(:subject).order(study_date: :desc).order(id: :desc)
+    user = current_user
+    @study_records = user.study_records.preload(:subject).order(study_date: :desc).order(id: :desc)
   end
 end
--- app/models/study_record.rb
+++ app/models/study_record.rb
@@ -19,6 +19,5 @@ class StudyRecord < ApplicationRecord
   belongs_to :user
   belongs_to :subject
 
-  validates :study_date, presence: true, comparison: { less_than_or_equal_to: Time.now }
   validates :study_time, presence: true, numericality: {only_integer: true, greater_than: 0}
 end
