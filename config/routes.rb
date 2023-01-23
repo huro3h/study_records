65256bd408c08aa30ec4a9f79a02e1d001907146
kotaro watanabe <ktr.w1011@gmail.com>
2023-01-23 21:33:57 +0900

    ログインしていないときの挙動を修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
   resources :tops, only: :index
-  resources :users
+  resources :users, only: [:new, :create, :show]
   resources :subjects, only: [:index, :new, :create, :show]
-  resources :study_records
+  resources :study_records, only: [:new, :create]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
