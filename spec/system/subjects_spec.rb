36a907e8deb87818c46ff3ed3bcda6f3bcf79268
aiit-misaki <a22z4my@aiit.ac.jp>
2023-01-25 12:28:33 +0900

    科目一覧の表示のテストが動くように修正

--- spec/system/subjects_spec.rb
+++ spec/system/subjects_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'subjects_path', type: :system, js: true do
+RSpec.describe 'Subjects', type: :system, js: true do
   describe '科目一覧ページ' do
     let!(:user) { create(:user) }
     #表示用のテストデータ
@@ -15,7 +15,7 @@ RSpec.describe 'subjects_path', type: :system, js: true do
       it '科目一覧が表示され、科目を追加するページに正常に遷移できること' do
         visit subjects_path
         expect(page).to have_current_path subjects_path
-        #expect(page).to have_content 'コラボレイティブ開発特論'
+        expect(page).to have_content 'コラボレイティブ開発特論'
         expect(page).to have_link href: new_subject_path
         click_on '科目を追加する'
       end
